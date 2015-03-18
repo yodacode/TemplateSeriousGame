1c7927a8490440e6ec8683f9530f522e174567c5
Benjamin de Vaublanc <benjamin.devaublanc@popsiit.net>
2015-03-18 16:01:49 +0100

    notif pusher

--- js/front.js
+++ js/front.js
@@ -1,6 +1,13 @@
 window.jQuery = $ = require('jquery');
 var bootstrap = require('bootstrap');
 
-$('.app-cache').on('click', function () {
-	$('body').removeClass('with-sidebar');
-});
+/* Quand je clique sur l'icône hamburger je rajoute une classe au body */
+   $('[data-btn-notif]').click(function(e){
+       e.preventDefault();
+       $('body').toggleClass('site-with-notif');
+   });
+
+   /* Je veux pouvoir masquer le menu si on clique sur le cache */
+   $('[data-site-cache]').click(function(e){
+       $('body').removeClass('site-with-notif');
+   });
